499de275732d19d67a7b19235d6b2ad7146c9a90
Julian Woelk <woelk@fundsaccess.com>
2021-07-24 13:16:47 +0200

    Add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faHamburger);
+
+Vue.component('fa-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
